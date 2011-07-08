90a25698cc20b4893e1e2d23803690df9108924e
David Lin <davll.xc@gmail.com>
2011-07-08 21:18:23 +0800

    add demo

---
+++ demo/common/include/demo_glut.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#if defined(__APPLE__)
+#  include <GLUT/GLUT.h>
+#else
+#  include <GL/glut.h>
+#endif
---
+++ demo/samples/basic_empty/main.cpp
@@ -0,0 +1,52 @@
+#include <cog/cog.h>
+#include <GLEW/glew.h>
+#include <demo_glut.h>
+
+static void _OnDisplayGLUT(void)
+{
+  // Render Now!
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  
+  // Swap Display Buffer
+  glutSwapBuffers();
+}
+
+static void _OnReshapeGLUT(int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+int main(int argc, char* argv[])
+{
+  glutInit(&argc, argv);
+  
+  glutInitWindowSize(1280, 720);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+  glutCreateWindow(argv[0]);
+  
+  { // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if(err!=GLEW_OK){
+      fprintf(stderr, "main: GLEW initialization failed.\n");
+      fprintf(stderr, "  Error: %s\n", glewGetErrorString(err));
+      return 1;
+    }
+    printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    printf("Status: OpenGL Version: %s\n", (char*)glGetString(GL_VERSION));
+    printf("Status: OpenGL Vendor: %s\n", (char*)glGetString(GL_VENDOR));
+    printf("Status: OpenGL Renderer: %s\n", (char*)glGetString(GL_RENDERER));
+  }
+  
+  // Install Repaint Event
+  glutDisplayFunc(_OnDisplayGLUT);
+  
+  // Install Resizing Event
+  glutReshapeFunc(_OnReshapeGLUT);
+  
+  // Enter Main Loop (NEVER RETURN)
+  glutMainLoop();
+  
+  return 0;
+}
+
