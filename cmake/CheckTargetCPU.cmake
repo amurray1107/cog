d220bddbb236bfeec0f586c0ddf9abbddce38a98
David Lin <davll.xc@gmail.com>
2011-07-16 12:02:03 +0800

    little fix

--- cmake/CheckTargetCPU.cmake
+++ cmake/CheckTargetCPU.cmake
@@ -8,6 +8,7 @@ try_run(run_result build_result ${bd} ${sc} RUN_OUTPUT_VARIABLE output)
 if(build_result AND (run_result EQUAL 0))
   set(TARGET_CPU_NAME "${output}")
   message("Processor: ${TARGET_CPU_NAME}")
+  # DEFINE __CPU__
   add_definitions("-D__${TARGET_CPU_NAME}__")
 else()
   unset(TARGET_CPU_NAME)
@@ -20,6 +21,8 @@ endif()
 ########### CONFIG
 
 if(${TARGET_CPU_NAME} MATCHES "^x86")
+  #################
+  #  X86 FAMILY   #
   message("Processor Family: Intel x86")
   add_definitions("-D__X86_SSE__" "-D__X86_SSE2__")
   if(${CMAKE_COMPILER_IS_GNUCXX})
@@ -27,11 +30,15 @@ if(${TARGET_CPU_NAME} MATCHES "^x86")
     add_definitions("-fabi-version=0")
     add_definitions("-Wall")
   endif()
+  #################
 elseif(${TARGET_CPU_NAME} MATCHES "^arm")
+  #################
+  #  ARM FAMILY   #
   message("Processor Family: ARM")
   add_definitions("-D__ARM_NEON__")
   if(${CMAKE_COMPILER_IS_GNUCXX})
     add_definitions("-mfpu=neon")
   endif()
+  #################
 endif()
 
