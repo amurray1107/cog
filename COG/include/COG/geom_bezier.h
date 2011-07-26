4d9cce841606a72b7c4f43a174909485be60ff57
David Lin <davll.xc@gmail.com>
2011-07-26 13:57:48 +0800

    Bezier Derivatives

--- COG/include/COG/geom_bezier.h
+++ COG/include/COG/geom_bezier.h
@@ -1,6 +1,6 @@
 namespace cog{
-  
-  template<typename T>
+
+  template<typename T, size_t stride>
   inline const basic_vector3<T> quadratic_bezier_curve
   (const basic_vector3<T> pt[3], T t)
   {
@@ -8,14 +8,14 @@ namespace cog{
     const T t1 = t;
     const T t01 = mul(t0, t1);
     
-    const basic_vector3<T> v0 = mul(t0, t0) * pt[0];
-    const basic_vector3<T> v1 = add(t01, t01) * pt[1];
-    const basic_vector3<T> v2 = mul(t1, t1) * pt[2];
+    const basic_vector3<T> v0 = mul(t0, t0) * pt[0*stride];
+    const basic_vector3<T> v1 = add(t01, t01) * pt[1*stride];
+    const basic_vector3<T> v2 = mul(t1, t1) * pt[2*stride];
     
     return v0 + v1 + v2;
   }
   
-  template<typename T>
+  template<typename T, size_t stride>
   inline const basic_vector3<T> cubic_bezier_curve
   (const basic_vector3<T> pt[4], T t)
   {
@@ -24,14 +24,51 @@ namespace cog{
     const T t01 = mul(t0, t1);
     const T t3 = add(t01, add(t01, t01));
     
-    const basic_vector3<T> v0 = mul(t0, mul(t0, t0)) * pt[0];
-    const basic_vector3<T> v1 = mul(t3, t0) * pt[1];
-    const basic_vector3<T> v2 = mul(t3, t1) * pt[2];
-    const basic_vector3<T> v3 = mul(t1, mul(t1, t1)) * pt[3];
+    const basic_vector3<T> v0 = mul(t0, mul(t0, t0)) * pt[0*stride];
+    const basic_vector3<T> v1 = mul(t3, t0) * pt[1*stride];
+    const basic_vector3<T> v2 = mul(t3, t1) * pt[2*stride];
+    const basic_vector3<T> v3 = mul(t1, mul(t1, t1)) * pt[3*stride];
     
     return v0 + v1 + v2 + v3;
   }
-  
+
+  //////////////////////////////
+
+  template<typename T, size_t stride>
+  inline const basic_vector3<T> quadratic_bezier_curve_dt
+  (const basic_vector3<T> pt[3], T t)
+  {
+    const T t0 = sub(const_<T>::one(), t);
+    const T t1 = t;
+
+    const basic_vector3<T> d0 = pt[1*stride] - pt[0*stride];
+    const basic_vector3<T> d1 = pt[2*stride] - pt[1*stride];
+
+    return add(t0, t0) * d0 + add(t1, t1) * d1;
+  }
+
+  template<typename T, size_t stride>
+  inline const basic_vector3<T> cubic_bezier_curve_dt
+  (const basic_vector3<T> pt[4], T t)
+  {
+    const T t0 = sub(const_<T>::one(), t);
+    const T t1 = t;
+    const T t01 = mul(t0, t1);
+    
+    const T t0s = mul(t0, t0);
+    const T t1s = mul(t1, t1);
+    const T t0sx3 = add(add(t0s, t0s), t0s);
+    const T t1sx3 = add(add(t1s, t1s), t1s);
+    const T t01x2 = add(t01, t01);
+    const T t01x6 = add(t01x2, add(t01x2, t01x2));
+    
+    const basic_vector3<T> d0 = pt[1*stride] - pt[0*stride];
+    const basic_vector3<T> d1 = pt[2*stride] - pt[1*stride];
+    const basic_vector3<T> d2 = pt[3*stride] - pt[2*stride];
+
+    return t0sx3 * d0 + t01x6 * d1 + t1sx3 * d2;
+  }
+
   //////////////////////////////
   
   template<typename T>
@@ -46,9 +83,9 @@ namespace cog{
     const T c1 = add(u01, u01);
     const T c2 = mul(u1, u1);
     
-    const basic_vector3<T> xu0 = quadratic_bezier_curve(pt[0], v);
-    const basic_vector3<T> xu1 = quadratic_bezier_curve(pt[1], v);
-    const basic_vector3<T> xu2 = quadratic_bezier_curve(pt[2], v);
+    const basic_vector3<T> xu0 = quadratic_bezier_curve<T, 1>(pt[0], v);
+    const basic_vector3<T> xu1 = quadratic_bezier_curve<T, 1>(pt[1], v);
+    const basic_vector3<T> xu2 = quadratic_bezier_curve<T, 1>(pt[2], v);
     
     return c0 * xu0 + c1 * xu1 + c2 * xu2;
   }
@@ -67,10 +104,10 @@ namespace cog{
     const T c2 = mul(u3, u1);
     const T c3 = mul(u1, mul(u1, u1));
     
-    const basic_vector3<T> xu0 = cubic_bezier_curve(pt[0], v);
-    const basic_vector3<T> xu1 = cubic_bezier_curve(pt[1], v);
-    const basic_vector3<T> xu2 = cubic_bezier_curve(pt[2], v);
-    const basic_vector3<T> xu3 = cubic_bezier_curve(pt[3], v);
+    const basic_vector3<T> xu0 = cubic_bezier_curve<T, 1>(pt[0], v);
+    const basic_vector3<T> xu1 = cubic_bezier_curve<T, 1>(pt[1], v);
+    const basic_vector3<T> xu2 = cubic_bezier_curve<T, 1>(pt[2], v);
+    const basic_vector3<T> xu3 = cubic_bezier_curve<T, 1>(pt[3], v);
     
     return c0 * xu0 + c1 * xu1 + c2 * xu2 + c3 * xu3;
   }
@@ -78,52 +115,96 @@ namespace cog{
   //////////////////////////////
 
   template<typename T>
-  inline const basic_vector3<T> cubic_hermite_curve
-  (const basic_vector3<T> pt[2], const basic_vector3<T> tangent[2], T t)
+  inline const basic_vector3<T> quadratic_bezier_patch_du
+  (const basic_vector3<T> pt[3][3], T u, T v)
   {
-    const T ts = mul(t, t);
-    const T tc = mul(t, ts);
-    const T ts2 = add(ts, ts);
-    const T ts3 = add(ts, ts2);
-    const T tc2 = add(tc, tc);
+    basic_vector3<T> du0, du1, du2;
 
-    const T c0 = add(sub(tc2, ts3), const_<T>::one());
-    const T c1 = add(sub(tc, ts2), t);
-    const T c2 = sub(tc, ts);
-    const T c3 = sub(ts3, tc2);
+    du0 = quadratic_bezier_curve_dt<T, 3>(pt[0]+0, u);
+    du1 = quadratic_bezier_curve_dt<T, 3>(pt[0]+1, u);
+    du2 = quadratic_bezier_curve_dt<T, 3>(pt[0]+2, u);
 
-    return c0 * pt[0] + c1 * tangent[0] + c2 * tangent[1] + c3 * pt[1];
+    const T v0 = sub(const_<T>::one(), v);
+    const T v1 = v;
+    const T v01 = mul(v0, v1);
+    
+    const T c0 = mul(v0, v0);
+    const T c1 = add(v01, v01);
+    const T c2 = mul(v1, v1);
+
+    return c0 * du0 + c1 * du1 + c2 * du2;
   }
 
   template<typename T>
-  inline void kochanek_bartels_curve
-  (const basic_vector3<T> pt[3], const T tpos[3], 
-   T tension, T continuity, T bias, 
-   basic_vector3<T>& oInTangent, basic_vector3<T>& oOutTangent )
+  inline const basic_vector3<T> quadratic_bezier_patch_dv
+  (const basic_vector3<T> pt[3][3], T u, T v)
   {
-    const basic_vector3<T> d0 = const_<T>::half() * (pt[1]-pt[0]);
-    const basic_vector3<T> d1 = const_<T>::half() * (pt[2]-pt[1]);
-    
-    const T nt = sub(const_<T>::one(), tension);
-    const T pc = add(const_<T>::one(), continuity);
-    const T nc = sub(const_<T>::one(), continuity);
-    const T pb = add(const_<T>::one(), bias);
-    const T nb = sub(const_<T>::one(), bias);
+    basic_vector3<T> dv0, dv1, dv2;
 
-    const T c0 = mul(nt, mul(pc, nb));
-    const T c1 = mul(nt, mul(nc, pb));
-    const T c2 = mul(nt, mul(nc, nb));
-    const T c3 = mul(nt, mul(pc, pb));
-    
-    const basic_vector3<T> vi = c0 * d1 + c1 * d0;
-    const basic_vector3<T> vo = c2 * d1 + c3 * d0;
-    const T dt0 = sub(tpos[1], tpos[0]);
-    const T dt1 = sub(tpos[2], tpos[1]);
-    const T dt01 = add(dt0, dt1);
+    dv0 = quadratic_bezier_curve_dt<T, 1>(pt[0], v);
+    dv1 = quadratic_bezier_curve_dt<T, 1>(pt[1], v);
+    dv2 = quadratic_bezier_curve_dt<T, 1>(pt[2], v);
 
-    oInTangent = vi * div(add(dt1, dt1), dt01);
-    oOutTangent = vo *  div(add(dt0, dt0), dt01);
+    const T u0 = sub(const_<T>::one(), u);
+    const T u1 = u;
+    const T u01 = mul(u0, u1);
+    
+    const T c0 = mul(u0, u0);
+    const T c1 = add(u01, u01);
+    const T c2 = mul(u1, u1);
+
+    return c0 * dv0 + c1 * dv1 + c2 * dv2;
   }
+
+  template<typename T>
+  inline const basic_vector3<T> cubic_bezier_patch_du
+  (const basic_vector3<T> pt[4][4], T u, T v)
+  {
+    basic_vector3<T> du0, du1, du2, du3;
+
+    du0 = quadratic_bezier_curve_dt<T, 4>(pt[0]+0, u);
+    du1 = quadratic_bezier_curve_dt<T, 4>(pt[0]+1, u);
+    du2 = quadratic_bezier_curve_dt<T, 4>(pt[0]+2, u);
+    du3 = quadratic_bezier_curve_dt<T, 4>(pt[0]+3, u);
+
+    const T v0 = sub(const_<T>::one(), v);
+    const T v1 = v;
+    const T v01 = mul(v0, v1);
+    const T v3 = add(v01, add(v01, v01));
+    
+    const T c0 = mul(v0, mul(v0, v0));
+    const T c1 = mul(v3, v0);
+    const T c2 = mul(v3, v1);
+    const T c3 = mul(v1, mul(v1, v1));
+
+    return c0 * du0 + c1 * du1 + c2 * du2 + c3 * du3;
+  }
+
+  template<typename T>
+  inline const basic_vector3<T> cubic_bezier_patch_dv
+  (const basic_vector3<T> pt[4][4], T u, T v)
+  {
+    basic_vector3<T> dv0, dv1, dv2, dv3;
+
+    dv0 = quadratic_bezier_curve_dt<T, 1>(pt[0], v);
+    dv1 = quadratic_bezier_curve_dt<T, 1>(pt[1], v);
+    dv2 = quadratic_bezier_curve_dt<T, 1>(pt[2], v);
+    dv3 = quadratic_bezier_curve_dt<T, 1>(pt[3], v);
+
+    const T u0 = sub(const_<T>::one(), u);
+    const T u1 = u;
+    const T u01 = mul(u0, u1);
+    const T u3 = add(u01, add(u01, u01));
+    
+    const T c0 = mul(u0, mul(u0, u0));
+    const T c1 = mul(u3, u0);
+    const T c2 = mul(u3, u1);
+    const T c3 = mul(u1, mul(u1, u1));
+
+    return c0 * dv0 + c1 * dv1 + c2 * dv2 + c3 * dv3;
+  }
+
+  //////////////////////////////
   
 }
 
--- COG/test/geom/bezier.cpp
+++ COG/test/geom/bezier.cpp
@@ -15,8 +15,12 @@ int main()
     };
     
     for(float t = 0.0f; t<=1.0f; t+=0.1f){
-      const vec3 vt = quadratic_bezier_curve(input, t);
+      const vec3 vt = quadratic_bezier_curve<F32, 1>(input, t);
+      const vec3 vd = quadratic_bezier_curve_dt<F32, 1>(input, t);
+
       if(!_test(vt.getZ(), 0.0f))
+        return 1;
+      if(!(vd.getX()> 0.0f))
         return 1;
     }
     
@@ -32,9 +36,14 @@ int main()
     };
     
     for(float t = 0.0f; t<=1.0f; t+=0.1f){
-      const vec3 vt = cubic_bezier_curve(input, t);
+      const vec3 vt = cubic_bezier_curve<F32, 1>(input, t);
+      const vec3 vd = cubic_bezier_curve_dt<F32, 1>(input, t);
+
       if(!_test(vt.getZ(), 0.0f))
         return 1;
+      if(!(vd.getX() > 0.0f))
+        return 1;
+
     }
   }
   
@@ -61,7 +70,14 @@ int main()
     for(float u = 0.0f; u<=1.0f; u+=0.1f)
       for(float v = 0.0f; v<=1.0f; v+=0.1f){
         const vec3 vt = quadratic_bezier_patch(input, u, v);
+        const vec3 vdu = quadratic_bezier_patch_du(input, u, v);
+        const vec3 vdv = quadratic_bezier_patch_dv(input, u, v);
+
         if(!_test(vt.getZ(), 0.0f))
+          return 1;
+        if(!(dot(cross(vdu, vdv), vec3(0.0f, 0.0f, 1.0f)) > 0.0f))
+          return 1;
+        if(!_test(normalize(cross(vdu, vdv)), vec3(0.0f, 0.0f, 1.0f)))
           return 1;
       }
     
@@ -99,58 +115,17 @@ int main()
     for(float u = 0.0f; u<=1.0f; u+=0.1f)
       for(float v = 0.0f; v<=1.0f; v+=0.1f){
         const vec3 vt = cubic_bezier_patch(input, u, v);
+        const vec3 vdu = cubic_bezier_patch_du(input, u, v);
+        const vec3 vdv = cubic_bezier_patch_dv(input, u, v);
+
         if(!_test(vt.getZ(), 0.0f))
+          return 1;
+        if(!(dot(cross(vdu, vdv), vec3(0.0f, 0.0f, 1.0f)) > 0.0f))
+          return 1;
+        if(!_test(normalize(cross(vdu, vdv)), vec3(0.0f, 0.0f, 1.0f)))
           return 1;
       }
     
-  }
-
-  // Test: Cubic Hermite Curve
-  {
-    const vec3 input[] = { 
-      vec3(0.0f), 
-      vec3(2.0f, 0.0f, 0.0f) 
-    };
-    const vec3 tangent[] = {
-      vec3(1.0f, 1.0f, 0.0f), 
-      vec3(-1.0f, 1.0f, 0.0f)
-    };
-    
-    for(float t = 0.0f; t<=1.0f; t+=0.1f){
-      const vec3 vt = cubic_hermite_curve(input, tangent, t);
-      if(!_test(vt.getZ(), 0.0f))
-        return 1;
-    }
-  }
-
-  // Test: Kochanek Bartels Curve
-  {
-    const vec3 input[] = { 
-      vec3(0.0f), 
-      vec3(1.0f, 0.0f, 0.0f), 
-      vec3(2.0f, 0.0f, 0.0f), 
-      vec3(3.0f, 0.0f, 0.0f)
-    };
-    const F32 tpos[] = { 0.0f, 1.0f, 2.0f, 3.0f };
-    vec3 tg[4];
-    
-    kochanek_bartels_curve(input, tpos, 0.0f, 0.0f, 0.0f, tg[0], tg[1]);
-    kochanek_bartels_curve(input+1, tpos, 0.0f, 0.0f, 0.0f, tg[2], tg[3]);
-
-    //for(int i=0;i<4;i++)
-      //printf("%f %f %f\n", tg[i].getX(), tg[i].getY(), tg[i].getZ());
-
-    for(float t = 0.0f; t<=1.0f; t+=0.1f){
-      const vec3 vt = cubic_hermite_curve(input+1, tg+1, t);
-      //printf("[%f %f %f]\n", vt.getX(), vt.getY(), vt.getZ());
-
-      if(!_test(vt.getX(), 1.0f+t))
-        return 1;
-      if(!_test(vt.getY(), 0.0f))
-        return 1;
-      if(!_test(vt.getZ(), 0.0f))
-        return 1;
-    }
   }
 
   // 
